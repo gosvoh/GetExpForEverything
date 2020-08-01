2b33ef8fc1d17fed6a6b9b01dc6f8b7c5b3999d6
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-08-01 18:16:30 +0300

    Added updating config on-the-fly feature

--- src/main/java/com/github/gosvoh/config/ConfigHelper.java
+++ src/main/java/com/github/gosvoh/config/ConfigHelper.java
@@ -1,23 +1,30 @@
 package com.github.gosvoh.config;
 
+import com.github.gosvoh.GetExpForEverything;
+import com.github.gosvoh.utils.Reference;
 import net.minecraftforge.fml.config.ModConfig;
+import org.apache.logging.log4j.Logger;
 
 public final class ConfigHelper {
-    private static ModConfig clientConfig;
+    private static final Logger LOGGER = GetExpForEverything.LOGGER;
 
     public static void bakeClient(final ModConfig config) {
-        clientConfig = config;
+        String prefix = Reference.MOD_ID + ".";
 
-        GetExpForEverythingConfig.baseExpToGain = ConfigHolder.COMMON.baseExpToGain.get();
-        GetExpForEverythingConfig.blocksNeedToDestroy = ConfigHolder.COMMON.blocksNeedToDestroy.get();
-        GetExpForEverythingConfig.countOfCraftedItems = ConfigHolder.COMMON.countOfCraftedItems.get();
-        GetExpForEverythingConfig.levelStep = ConfigHolder.COMMON.levelStep.get();
-        GetExpForEverythingConfig.multiplierForLevelStep = ConfigHolder.COMMON.multiplierForLevelStep.get();
-        GetExpForEverythingConfig.blackListBlocks = ConfigHolder.COMMON.blackListBlocks.get();
-        GetExpForEverythingConfig.blackListCraftedItems = ConfigHolder.COMMON.blackListCraftedItems.get();
-        GetExpForEverythingConfig.blackListBlockTags = ConfigHolder.COMMON.blackListBlockTags.get();
-        GetExpForEverythingConfig.blackListItemTags = ConfigHolder.COMMON.blackListItemTags.get();
+        GetExpForEverythingConfig.baseExpToGain = config.getConfigData().getInt(prefix + "base_experience");
+        GetExpForEverythingConfig.blocksNeedToDestroy = config.getConfigData().getInt(prefix + "blocks_need_to_destroy");
+        GetExpForEverythingConfig.itemsNeedToCraft = config.getConfigData().getInt(prefix + "items_to_craft");
+        GetExpForEverythingConfig.levelStep = config.getConfigData().getInt(prefix + "level_step");
+        GetExpForEverythingConfig.multiplierForLevelStep = config.getConfigData().getInt(prefix + "multiplier_for_level_step");
+        GetExpForEverythingConfig.blackListBlocks = config.getConfigData().get(prefix + "black_list_blocks");
+        GetExpForEverythingConfig.blackListCraftedItems = config.getConfigData().get(prefix + "black_list_items");
+        GetExpForEverythingConfig.blackListBlockTags = config.getConfigData().get(prefix + "black_list_block_tags");
+        GetExpForEverythingConfig.blackListItemTags = config.getConfigData().get(prefix + "black_list_item_tags");
 
+        Reference.countOfBrokenBlocks = 0;
+        Reference.countOfCraftedItems = 0;
+
+        LOGGER.debug(Reference.MOD_NAME + " configuration file was changed!");
     }
 
     private static void setValueAndSave(final ModConfig modConfig, final String path, final Object newValue) {
