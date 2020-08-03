695911e1aad665eff715f5f91db4007311d48dfe
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-08-03 10:56:31 +0300

    Removed useless methods that causes crash

--- src/main/java/com/github/gosvoh/GetExpForEverything.java
+++ src/main/java/com/github/gosvoh/GetExpForEverything.java
@@ -2,16 +2,9 @@ package com.github.gosvoh;
 
 import com.github.gosvoh.config.ConfigHolder;
 import com.github.gosvoh.utils.Reference;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,29 +17,6 @@ public class GetExpForEverything {
         LOGGER.info(Reference.MOD_NAME + " version " + Reference.VERSION +
                     " by " + Reference.AUTHOR + " started up.");
 
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        bus.addListener(this::setup);
-        bus.addListener(this::enqueueIMC);
-        bus.addListener(this::processIMC);
-        bus.addListener(this::doClientStuff);
-        bus.addListener(this::onServerStarting);
-
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHolder.COMMON_SPEC);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-    }
-
-    public void onServerStarting(FMLServerStartingEvent event) {
     }
 }
